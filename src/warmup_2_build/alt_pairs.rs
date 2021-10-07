c4e5ec1ffdd8cb8514ae8612869f363d5939f037
Frenzy <frenzyexists@gmail.com>
2021-10-07 00:26:05 -0400

    A

---
+++ src/warmup_2_build/alt_pairs.rs
@@ -0,0 +1,16 @@
+/**
+ * Given a string, return a string made of 
+ * the chars at indexes 0,1, 4,5, 8,9 ...
+ * so "kittens" yields "kien".
+ *
+ * altPairs("kitten") → "kien"
+ * altPairs("Chocolate") → "Chole"
+ * altPairs("CodingHorror") → "Congrr"
+ *
+ */
+
+// Pairs are like n, n+1; skip n+2 and  n+3; n+4
+// which means that every n+2 and n+3 is skipped
+pub fn alt_pairs(string: &str) {
+    
+}
---
+++ src/warmup_2_build/string_times.rs
@@ -0,0 +1,12 @@
+/**
+ * Given a string and a non-negative int n, 
+ * return a larger string that is n copies 
+ * of the original string.
+ *
+ * stringTimes("Hi", 2) → "HiHi"
+ * stringTimes("Hi", 3) → "HiHiHi"
+ * stringTimes("Hi", 1) → "Hi"
+ */
+pub fn string_times(string: &str) -> String {
+    return "".to_string();
+}
